64164b541c4720c2733355c33ce462734de787da
beapl <blbealeite@gmail.com>
2025-05-05 23:35:39 +0100

    ##tela !!!

--- TestePlayza/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/TestePlayza.AssemblyInfo.cs
+++ TestePlayza/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/TestePlayza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestePlayza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+909b2ca9b44df6bf9be9141c1e0be546e6956f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+86d3cb558ded7aa088586c9a2cb84fd2c8ce481a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestePlayza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestePlayza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
