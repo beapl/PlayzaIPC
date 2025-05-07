6bc67e3dfb947173e096573ce8a8d0d2439fe906
beapl <blbealeite@gmail.com>
2025-05-08 00:47:49 +0100

    main page já 90% pronta

--- TestePlayza/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TestePlayza.AssemblyInfo.cs
+++ TestePlayza/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TestePlayza.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestePlayza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+17aee0afa74201f44875f4c652009626042eabee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+16bc64ed30478bc765f9b9aa64bc0b297d026c34")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestePlayza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestePlayza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
